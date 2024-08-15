150146d391ebffdae2a975a86d2b9660dcf2f670
richard gowers <richardjgowers@gmail.com>
2024-08-15 15:59:21 +0100

    hollow out XDRUnpacker to instead devolve functionality to XDRThing

--- omnireader/xdrfile.pyx
+++ omnireader/xdrfile.pyx
@@ -37,13 +37,23 @@ cdef extern from "omnireader.h":
         cbool is_big_endian() const
         void set_stream(const char* src)
         void set_double_precision(cbool toggle)
+        cbool get_double_precision()
         void set_is_2020(cbool toggle)
+        size_t get_bool(cbool &output)
+        size_t get_ushort(unsigned int &output)
+        size_t get_uchar(unsigned int &output)
+        size_t get_real(double &output)
         size_t get_float(float &output)
         size_t get_double(double &output)
         size_t get_int32(int &output)
         size_t get_uint32(unsigned int &output)
         size_t get_int64(int64_t &output)
         size_t get_uint64(uint64_t &output)
+        stdstring do_string()
+        void skip_real(size_t n)
+        void skip_int(size_t n)
+        void skip_ushort(size_t n)
+        void skip_bool(size_t n)
 
 cdef extern from "tpr_settings.h":
     cset[int] SUPPORTED_VERSIONS
@@ -171,19 +181,14 @@ cdef class XDRUnpacker:
     cdef char *buffer
     cdef char *ptr
     cdef int length
-    cdef cbool is_2020
-    cdef cbool double_prec
 
     def __cinit__(self):
         self.buffer = NULL
         self.ptr = NULL
         self.length = 0
-        self.double_prec = False
 
     def __init__(self, bytes data):
         self._set_buffer(data, len(data))
-        self.double_prec = False
-        self.is_2020 = False
 
     def __dealloc__(self):
         if self.buffer != NULL:
@@ -205,17 +210,6 @@ cdef class XDRUnpacker:
     cpdef stdstring read(self, int n):
         return stdstring(self.ptr, n)
 
-    def reset(self, bytes data):
-        self.double_prec = False
-        self.is_2020 = False
-        self._set_buffer(data, len(data))
-
-    cpdef int get_position(self):
-        return self.ptr - self.buffer
-
-    cpdef void set_position(self, int pos):
-        self.ptr = self.buffer + pos
-
     cpdef void set_is_2020(self, cbool i):
         """Toggle 2020 behaviour
 
@@ -232,31 +226,20 @@ cdef class XDRUnpacker:
          - unpack_bool
            - post 20202 uses 1 byte not 4 per bool
         """
-        self.is_2020 = i
+        self.converter.set_is_2020(i)
 
     cpdef void set_is_double(self, cbool is_double):
         """Toggles the behaviour of unpack_real"""
         self.converter.set_double_precision(is_double)
 
     cpdef double unpack_real(self):
-        if self.double_prec:
-            return self.unpack_double()
-        else:
-            return self.unpack_float()
+        cdef double ret
+        self.converter.get_real(ret)
+        return ret
 
     cpdef stdstring do_string(self):
         """This is different to unpack_string."""
-        cdef int i32
-        cdef unsigned long long i64
-
-        if self.is_2020:
-            i64 = self.unpack_uint64()
-            return self.unpack_fstring(i64)
-        else:
-            # this seems to mean there's a useless int before each string
-            # as unpack_string reads the length itself
-            i32 = self.unpack_int()
-            return self.unpack_string()
+        return self.converter.do_string()
 
     def get_buffer(self) -> bytes:
         return b''
@@ -264,200 +247,54 @@ cdef class XDRUnpacker:
     def done(self) -> bool:
         return self.get_position() == self.length
 
-    cpdef unsigned int unpack_uint(self):
-        cdef unsigned int i=0
-        cdef size_t ret
-
-        ret = self.converter.get_uint32(self.ptr, i)
-
-        self.ptr += ret
-
-        return i
-
     cpdef int unpack_int(self):
         cdef int i=0
-        cdef size_t ret
-
-        ret = self.converter.get_int32(self.ptr, i)
-
-        self.ptr += ret
-
+        self.converter.get_int32(i)
         return i
 
     cpdef long long unpack_int64(self):
-        cdef long long i=0
-        cdef size_t ret
-
-        ret = self.converter.get_int64(self.ptr, i)
-
-        self.ptr += ret
-
-        return i
-
-    cpdef unsigned long long unpack_uint64(self):
-        cdef unsigned long long i=0
-        cdef size_t ret
-
-        ret = self.converter.get_uint64(self.ptr, i)
-
-        self.ptr += ret
-
-        return i
-
-    unpack_enum = unpack_int
+        cdef int64_t ret
+        self.converter.get_int64(ret)
+        return ret
 
     cpdef cbool unpack_bool(self):
         # this also varies with version
-        cdef int a
-        cdef char b
-        cdef size_t ret
-
-        if self.is_2020:
-            b = self.ptr[0]
-            self.ptr += 1
-
-            return b != 0
-        else:
-            ret = self.converter.get_int32(self.ptr, a)
-            self.ptr += ret
-
-            return a != 0
-
-    def unpack_uhyper(self):
-        pass
+        cdef cbool ret
+        self.converter.get_bool(ret)
+        return ret
 
     cpdef float unpack_float(self):
-        cdef float i=0
-        cdef size_t ret
-
-        ret = self.converter.get_float(self.ptr, i)
-
-        self.ptr += ret
-
-        return i
+        cdef float ret
+        self.converter.get_float(ret)
+        return ret
 
     cpdef double unpack_double(self):
-        cdef double i=0
-        cdef size_t ret
-
-        ret = self.converter.get_double(self.ptr, i)
-
-        self.ptr += ret
-
-        return i
-
-    @cython.cdivision(True)  # we want floor division inside here
-    cpdef stdstring unpack_fstring(self, int n):
-        cdef int j
-
-        s = stdstring(self.ptr, n)
-
-        if self.is_2020:
-            # this version seems to not pad strings to 4 byte boundaries
-            j = n
-        else:
-            # advance pointer to multiple of n bytes
-            j = (n + 3) / 4 * 4
-
-        self.ptr += j
-
-        return s
-
-    unpack_fopaque = unpack_fstring
-
-    cpdef stdstring unpack_string(self):
-        cdef int i
-        i = self.unpack_int()
-
-        return self.unpack_fstring(i)
-
-    unpack_opaque = unpack_string
-
-    unpack_bytes = unpack_string
-
-    def unpack_list(self, unpack_item):
-        pass
-
-    def unpack_farray(self, unpack_item):
-        pass
-
-    def unpack_array(self, unpack_item):
-        pass
+        cdef double ret
+        self.converter.get_double(ret)
+        return ret
 
     cpdef char unpack_uchar(self):
-        cdef char val
-        cdef int i
-
-        if self.is_2020:
-            # these are one byte long
-            val = self.ptr[0]
-            self.ptr += 1
-        else:
-            # older version, 4 bytes per char
-            i = self.unpack_int()
-            val = i
-
-        return val
+        cdef unsigned char i
+        self.converter.get_uchar(i)
+        return i
 
     cpdef unsigned int unpack_ushort(self):
-        cdef int i
-        cdef unsigned short j
-        cdef size_t ret
-        cdef char tmp[2]
-
-        if self.is_2020:
-            # this uses 2 bytes per short, still in network (BE) representation
-            if self.converter.is_big_endian():
-                tmp[0] = self.ptr[1]
-                tmp[1] = self.ptr[0]
-            else:
-                tmp[0] = self.ptr[0]
-                tmp[1] = self.ptr[1]
-
-            j = dereference(<unsigned short*>&tmp)
-
-            self.ptr += 2
-
-            return j
-        else:
-            return self.unpack_int()
-
-    cdef void skip(self, int amount):
-        """skip a number of bytes ahead
-        
-        saves doing byte swaps on stuff we're not going to look at
-        """
-        self.ptr += amount
+        cdef unsigned int ret
+        self.converter.get_ushort(ret)
+        return ret
 
     cdef void skip_real(self, int n):
         """skip n reals"""
-        self.skip(n * 4)
-        if self.double_prec:
-            self.skip(n * 4)
+        self.converter.skip_real(n)
 
     cdef void skip_int32(self, int n):
-        self.skip(n * 4)
-
-    cdef void skip_int64(self, int n):
-        self.skip(n * 8)
-
-    cdef void skip_float(self, int n):
-        self.skip(n * 4)
-
-    cdef void skip_double(self, int n):
-        self.skip(n * 8)
+        self.converter.skip_int(n)
 
     cdef void skip_ushort(self, int n):
-        if self.is_2020:
-            self.skip(n * 2)
-        else:
-            self.skip(n * 4)
+        self.converter.skip_ushort(n)
 
     cdef void skip_bool(self, int n):
-        if self.is_2020:
-            self.skip(n)
-        else:
-            self.skip(n * 4)
+        self.converter.skip_bool(n)
 
 
 cdef class TpxHeader:
